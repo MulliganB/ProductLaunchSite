730ab907373297215ec39c64b6a0ce3768bc6c03
Benjamin Mulligan <benjaminmulligan79@gmail.com>
2018-01-14 14:59:15 +0000

    basic file excluding images
    no images included

---
+++ indicator.js
@@ -0,0 +1,28 @@
+$(document).ready( function() {
+  $(".indicator").css('color', '#ff8a80');
+  $(".tabs.tabs-transparent .indicator").css('background-color', '#ff8a80');
+});
+$(".p1").click(
+   function () {
+      $(".indicator").css('color', '#ff8a80');
+      $(".tabs.tabs-transparent .indicator").css('background-color', '#ff8a80');
+   }
+);
+$(".p2").click(
+   function () {
+      $(".indicator").css('color', '#009688');
+      $(".tabs.tabs-transparent .indicator").css('background-color', '#009688');
+   }
+);
+$(".p3").click(
+   function () {
+      $(".indicator").css('color', '#ffd180');
+      $(".tabs.tabs-transparent .indicator").css('background-color', '#ffd180');
+   }
+);
+$(".p4").click(
+   function () {
+      $(".indicator").css('color', '#82b1ff');
+      $(".tabs.tabs-transparent .indicator").css('background-color', '#82b1ff');
+   }
+);
